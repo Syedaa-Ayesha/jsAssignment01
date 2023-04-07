36e35e91a447a92d41688ad38335400831f0af0e
Syeda Ayesha <syedaa923@gmail.com>
2023-04-07 19:30:27 +0500

    main

---
+++ app.js
@@ -0,0 +1,96 @@
+//  -----------------------------Q#01 --------------------------
+
+// let userInput  = +prompt("Enter first number");
+// let userInput1  = +prompt("Enter second number");
+//  if (userInput >= userInput1){
+//     console.log(userInput + " is a greater number");
+//  } else{
+//     console.log(userInput1 + " is a greater number");
+//  }
+
+//  -----------------------------Q#02--------------------------
+
+// let userInput  = +prompt("Enter a number");
+
+// if (userInput < 0){
+//     console.log("The number is negative(-)");
+// } else{
+//     console.log("The number is positive(+)");
+// }
+
+//  -----------------------------Q#03--------------------------
+
+// let userInput  = +prompt("Enter the first number");
+// let userInput1 = +prompt("Enter the second number");
+// let userInput2 = +prompt("Enter the third number");
+// let userInput3 = +prompt("Enter the fourth number");
+// let userInput4 = +prompt("Enter the fifth number");
+
+// if (userInput >= userInput1 ) {
+//     console.log(userInput + " is a greater number");
+// }
+// else if (userInput1 >= userInput2 ) {
+//     console.log(userInput1 + " is a greater number");
+// }
+// else if (userInput2 >= userInput3 ) {
+//     console.log(userInput2 + " is a greater number");
+// }
+// else if (userInput3 >= userInput4 ) {
+//     console.log(userInput3 + " is a greater number");
+// } else{
+//     console.log(userInput4 + " is a greater number");
+// }
+
+//   -----------------------------Q#04--------------------------
+
+// for(let i = 0 ; i<=15 ; i++){
+//     if(i % 2 === 0){
+//         document.write(i + " is a even <br><br>");
+//     } else{
+//         document.write(i + " is a odd <br><br>");
+//     }
+// }
+
+//   -----------------------------Q#05--------------------------
+
+// let sub1 = 60;
+// let sub2 = 70;
+// let sub3 = 68;
+// let sub4 = 91;
+// let sub5 = 82;
+// let averageMarks = (sub1 + sub2 + sub3 + sub4 + sub5)/5;
+// console.log(averageMarks);
+// if(averageMarks < 70){
+//     console.log("D");
+// }else if(averageMarks < 80){
+//     console.log("C");
+// }else if(averageMarks < 90){
+//     console.log("B");
+// }else if(averageMarks < 100){
+//     console.log("A");
+// } else{
+//     console.log("F");
+// }
+
+//  -----------------------------Q#06--------------------------
+
+// for (let i = 1; i <= 100 ; i++){
+//   if(i % 15 === 0){
+//     document.write("FizzBuzz <br>");
+//   } else if(i % 5 === 0 ){
+//     document.write("Buzz <br>");
+//   } else if(i % 3 === 0){
+//     document.write("Fizz <br>");
+//   }else {
+//     document.write(i + "<br>");
+//   }
+// }
+
+//  -----------------------------Q#07--------------------------
+
+// for (let i = 0 ; i <= 5; i++){
+//     for (let j = 0 ; j<= i-1 ; j++) {
+//         document.write(" *");
+//     }
+//  document.write("<br>");
+// }
